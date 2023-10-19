2fb9cf9efbb89d3f884d216d5269953671548a4f
HHB <bcl0206@gmail.com>
2023-10-19 17:16:28 +0900

    feat: Access-Control-Allow-Origin 설정

--- router/s3uploader.js
+++ router/s3uploader.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const generateUploadURL = require('./s3.js')
 
 router.get('/', async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   const url = await generateUploadURL()
   res.send({url})
 })
